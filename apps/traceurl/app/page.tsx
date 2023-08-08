785c15719ac496fcf6272a12c757f4d30f7c4e66
sinchang <sinchangwen@gmail.com>
2023-08-08 14:23:41 +0800

    f

--- apps/traceurl/app/page.tsx
+++ apps/traceurl/app/page.tsx
@@ -1,10 +1,11 @@
-import { Button, Header } from "ui";
+import { Input } from "ui";
 
 export default function Page() {
   return (
-    <>
-      <Header text="Web" />
-      <Button />
-    </>
+    <div className="m-auto mt-10 text-center max-w-[500px]">
+      <h1 className="text-4xl font-bold">Trace URL</h1>
+      <p className="m-2 text-left">Trace URL helps you expand shortened URL into original URL or trace any URL with redirections towards the destination.</p>
+      <Input className="mt-5" />
+    </div>
   );
 }
---
+++ packages/ui/src/Input.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+interface Props {
+  children?: React.ReactNode;
+  className?: string
+  type?: React.HTMLInputTypeAttribute
+}
+
+export const Input = ({ children, className, type = 'text' }: Props) => {
+  return <input type={type} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
+};
